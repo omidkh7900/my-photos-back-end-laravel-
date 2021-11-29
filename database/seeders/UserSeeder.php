6a03436e10aac66208e332e406d8464c00b7bcc2
user <omidkheirabady@protonmail.com>
2021-11-29 18:42:42 +0500

    initialise laravel project

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'  =>  'user',
+            'email' =>  'user@email.com',
+            'email_verified_at' => now(),
+            'password'  =>  bcrypt('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory(10)->create();
+    }
+}
